c8f51ec24f79e924a9d73ac9163f1775e8b7b080
Baltasar Barbaglia <baltasarbarbaglia@gmail.com>
2022-02-04 18:22:13 -0300

    Commit 2: Nueva rama creada llamada 'test'. Creada carpeta 'components' y dentro de ella los archivos para la NavBar

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems=[
+    {
+        title: 'Inicio',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Productos',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'FAQs',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Iniciar Sesión',
+        url: '#',
+        cName:'nav-links'
+    },
+]
